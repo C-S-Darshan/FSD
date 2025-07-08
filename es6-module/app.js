184f0507e035c0cb619e7f63a3645b5d8610c789
C S Darshan <96053838+C-S-Darshan@users.noreply.github.com>
2025-07-08 07:29:21 +0000

    es6 module assignment

---
+++ es6-module/app.js
@@ -0,0 +1,16 @@
+import {add, substract} from './math.js'
+import sayHello from './greet.js'
+import { add as  sum, substract as diff} from './mathOperations.js';
+
+console.log("************* Demo **************")
+console.log("Add: ",add(2,3));
+console.log("Subtract: ",substract(2,3));
+console.log(sayHello("Darshan"));
+
+console.log("************* Assignment 1 **************")
+console.log("Sum: ",sum(2,3));
+console.log("Difference: ",diff(2,3));
+
+console.log("************* Assignment 2 **************")
+console.log("Sum: ",sum(2,3));
+console.log("Difference: ",diff(2,3));
